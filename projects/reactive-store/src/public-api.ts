367e90ceaca60340d6ab9f654ae9d9c8ff864d8d
Bill Cheng <cccheng118@yahoo.com>
2020-02-20 21:05:06 -0600

    Add clearSideEffects

--- projects/reactive-store/src/public-api.ts
+++ projects/reactive-store/src/public-api.ts
@@ -6,7 +6,11 @@ export { addReducer, dispatchAction } from './lib/core';
 export { HookCallBack, enableConsoleLog } from './lib/core-hook';
 export { createAction, CreatedAction } from './lib/createAction';
 export { Effect } from './lib/effect-decorator';
-export { addSideEffect, removeSideEffect } from './lib/effects-core';
+export {
+  addSideEffect,
+  removeSideEffect,
+  clearSideEffects
+} from './lib/effects-core';
 export { InitialState } from './lib/initial-state-decorator';
 export { Reducer } from './lib/reducer-decorator';
 export { Store } from './lib/store';
